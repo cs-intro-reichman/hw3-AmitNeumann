c114e0b1b821e4c7d16390eee340991033fe70d4
Amit Neumann <amit.neumann@post.runi.ac.il>
2024-11-23 18:44:19 +0200

    only algebra

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,192 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if(x2>=0) {
+			int x3=x1;
+			for (int i=0;i<x2;i++) {
+				x3++;
+			}
+			return x3;
+		} else { 
+			int x3=x1;
+			for (int i=0;i>x2;i--) {
+				x3--;
+			}
+			return x3;
+		}
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3;
+		if(x1>=0&&x2>=0){ // both are positive
+			x3=x1;
+			for (int i=0;i<x2;i++) {
+				x3--;
+			}
+		} else { // one of them is <0
+			x3=x1;
+			if (x2<0) { // -(-x2)== +x2
+				for (int i=0;i>x2;i--) {
+					x3++;
+				}
+			} else { // x1<0 && x2>0
+				for(int i=0; i<x2;i++) {
+					x3--;
+				}
+			}
+			
+		}
+		return x3;
 	}
+		
+	
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int n=x1,restartN=x1;
+		if (x1==0||x2==0) {
+			return 0;
+		} else { 
+			if (x1<0&&x2<0) { // both <0
+				int absX1=0,absX2=0;
+				while(x1!=0) {
+					absX1++;
+					x1++;
+				} while(x2!=0) {
+					absX2++;
+					x2++;
+					}
+				x1=absX1;
+				x2=absX2;
+				n=x1;
+				restartN=x1;
+			}
+			if(x1>0&&x2>0){
+				for(int i=1;i<x2;i++) {
+					n=restartN;
+					while(n!=0){
+						x1++;
+						n--;
+					}
+				}
+			return x1;
+			} else { // at least one is <0
+				if (x2>0) { // and x1<0
+					for(int i=0;i<x2;i++){
+						n=restartN;
+						while(n!=0){
+							x1--;
+							n++;
+						}
+					}
+				return x1;
+				} else { if(x1>0) {  // and x2<0
+							n=x2;
+							restartN=x2;
+							for(int i=0;i<x1;i++){
+								n=restartN;
+								while(n!=0){
+									x2--;
+									n++;
+								}
+							}
+						} 
+				}
+			}
+		}
+		return x2;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+		if (x==0) {
+			return 0;
+		}
+		else { if(n==0) {
+				return 1;
+			}
+		}
+		int sum=x;
+		for(int i=1;i<n;i++) {
+			sum=times(sum, x);
+			}
+			return sum;
+		}
+	
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 0) {
+			throw new ArithmeticException("Division by zero is undefined");
+		}
+
+		// Determine if the result should be negative
+		boolean negativeResult = false;
+		if (x1 < 0) {
+			x1 = minus(0, x1); // Make a positive
+			negativeResult = true;
+		}
+		if (x2 < 0) {
+			x2 = minus(0, x2); // Make b positive
+			if (negativeResult) {
+				negativeResult = false; // Negative divided by negative gives positive
+			} else {
+				negativeResult = true; // Positive divided by negative gives negative
+			}
+		}
+		//  division
+		int result = 0;
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+			result = plus(result, 1);
+		}
+	
+		// Return result with correct sign
+		if (negativeResult) {
+			return minus(0, result);
+		} else {
+			return result;
+		}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
-
+		if (x2 == 0) {
+			throw new ArithmeticException("Modulo by zero is undefined");
+		}
+		// Determine if the remainder should be negative
+		boolean negativeResult = false;
+		if (x1 < 0) {
+			x1 = minus(0, x1); // Make x1 positive
+			negativeResult = true;
+		}
+		if (x2 < 0) {
+			x2 = minus(0, x2); // Make x2 positive
+		}
+		//modulo 
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+		}
+	
+		// Return remainder with correct sign
+		if (negativeResult) {
+			return minus(0, x1);
+		} else {
+			return x1;
+		}
+	}
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		if (x < 0) {
+			throw new ArithmeticException("Square root of a negative number is undefined");
+		}
+		// Start finding the square root
+		int result = 0;
+		while (times(result, result) <= x) {
+			result = plus(result, 1);
+		}
+		// -1 for the extra iteration
+		return minus(result, 1);
 	}	  	  
 }
